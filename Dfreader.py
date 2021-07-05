54ed44de5eb7bf27d52972a5ecb6b7ae8783de14
rahulvenugopal84 <rahulvenugopal84>
2021-07-05 21:39:05 +0000

    master

--- Dfreader.py
+++ Dfreader.py
@@ -7,10 +7,6 @@ newschema="lb_income_band long ,ib_lb int , ib_up int"
 incomeBandDf=spark.read.schema(newschema).option("sep","|").format('csv').load('/FileStore/tables/retailer/data/income_band.dat')
 incomeBandDf.printSchema()
 
-print("this is from second branch")
-
-print("This is from yout first branch")
-
 
 # COMMAND ----------
 
